ddc526f4f1acb974721c9114dbc45fcc73d8c84a
jasmine Magdi <jesojolay@yahoo.com>
2017-09-11 13:05:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-     var grand =document.querySelector("#grand-node")
+     var grand =document.querySelector("#grand-node");
      console.log(grand.querySelector("div"));
 }
 deepestChild();
