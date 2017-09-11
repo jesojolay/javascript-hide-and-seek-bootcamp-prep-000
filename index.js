b9939dbb4579ebfa09825b884c7e247dd6bd9862
jasmine Magdi <jesojolay@yahoo.com>
2017-09-11 12:46:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-getFirstSelector(selector){
+function getFirstSelector(selector){
         return document.querySelector(selector);
 }
+
+function nestedTarget(nested, target){
+    return document.querySelector(nested).querySelector(target);
+}
+
+
