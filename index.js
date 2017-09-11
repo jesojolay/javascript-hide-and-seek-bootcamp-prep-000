17be4be43ca499f03b053aec350c674d36485745
jasmine Magdi <jesojolay@yahoo.com>
2017-09-11 13:44:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested").querySelector(".target");
+}
+
+function increaseRankBy(n) {
+  var lis = document.querySelector("#app").querySelectorAll(".ranked-list li");
+  for (let i = 0; i < lis.length; i++) {
+    var num = parseInt(lis[i].innerHTML);
+    lis[i].innerHTML = (num += n).toString();
+  }
+}
+
+function deepestChild() {
+    var grand = document.querySelector("#grand-node");
+    var arr = grand.querySelectorAll("div");
+    return arr[3];
+
+}
