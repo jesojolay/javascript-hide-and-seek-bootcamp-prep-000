aada0e848fdaf9ade81c7a659f8a8839ac7753ed
jasmine Magdi <jesojolay@yahoo.com>
2017-09-11 13:16:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function increaseRankBy(n){
 function deepestChild(){
      var grand = document.querySelector("#grand-node");
      var div = grand.children[3];
-     var boo = div.querySelector("div").innerHTML;
+     var boo = div.querySelector("div");
      return boo;
 
 
